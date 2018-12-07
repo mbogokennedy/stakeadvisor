46baf9ebf70d11a28cfa827041ef9ddaae247526
Kennedy Mbogo <mbogokennedy@gmail.com>
2018-12-07 15:58:58 +0300

    changes in static files

--- stakeadvisor/settings.py
+++ stakeadvisor/settings.py
@@ -45,7 +45,7 @@ DATABASES = {
 }
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ["stakeadvisor.herokuapp.com", "127.0.0.1"]
+ALLOWED_HOSTS = [".herokuapp.com", ".127.0.0.1"]
 
 
 # Application definition
@@ -176,5 +176,19 @@ LOGOUT_REDIRECT_URL = LOGIN_URL
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
 
