8412ef7e7b823165486bb7f3bf1293a45210cebd
Kennedy Mbogo <mbogokennedy@gmail.com>
2018-12-07 13:03:27 +0300

    Changes on Base html

--- stakeadvisor/settings.py
+++ stakeadvisor/settings.py
@@ -144,13 +144,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 IMPORT_EXPORT_USE_TRANSACTIONS = True
 
