69fbf83ff65860f957f880b2ea1319cfd13542d8
Kennedy Mbogo <mbogokennedy@gmail.com>
2018-12-07 20:02:33 +0300

    changes in setting storage

--- stakeadvisor/settings.py
+++ stakeadvisor/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUD_NAME'),
     'API_KEY': config('CLOUD_API_KEY'),
-    'API_SECRET': config('CLOUD_API_SECRET')'
+    'API_SECRET': config('CLOUD_API_SECRET'),
 }
 
 
